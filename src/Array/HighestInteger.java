d399dcea3b88d3124c61bfe55021987923287b4f
Abzalbek Issabekov <abzalbekissabekov@Abzalbeks-MacBook-Pro.local>
2020-05-11 17:35:13 -0700

    second highest integer

--- src/Array/HighestInteger.java
+++ src/Array/HighestInteger.java
@@ -4,17 +4,23 @@ import org.junit.Test;
 
 public class HighestInteger {
 
-
+/*
+searching second highest integer in an array
+ */
     public int findHighestNumber(int[] a) {
 
-        int max = Integer.MIN_VALUE;
+        int first, second;
+        first = second = Integer.MIN_VALUE;
 
         for (int i = 0; i < a.length; i++) {
-            if (a[i] > max) {
-                max = a[i];
+            if (a[i] > first) {
+                second=first;
+                first = a[i];
+            }if(a[i]>second && a[i]!=first){
+                second=a[i];
             }
         }
-        return max;
+        return second;
     }
 
     @Test
